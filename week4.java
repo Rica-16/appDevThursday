9a943a868bdff9037ef44ea48ff5db57d1e9a164
Raquino-Michaela <mraquino@paterostechnologicalcollege.edu.ph>
2025-03-02 16:24:01 +0800

    Raquino,Michaela

--- week4.java
+++ week4.java
@@ -7,32 +7,25 @@ public class week4 {
         //student1.display();
        // Add your method below
         // More students will add their method calls here...
-
-
-        Hallig.display()
-
-         Piano.display();
-         Geringer.display();
-         ClarkKurt.display();
-         Hernandez.display(); 
-         Piano.display();
-
-Oliveros.display();
-
-Hallig.display()
-
-Pascua.display();
-	Smith.display();
-	Piano.display();
-	Geringer.display();
-	ClarkKurt.display();
-	Hernandez.display(); 
-	Piano.display();
-	KPClark.display();
-
-	Bermejo.display();
-
-Santander.display()
+	     Raquino.display();
+	    Hallig.display();
+	    Piano.display();
+	    Geringer.display();
+	    ClarkKurt.display();
+	    Hernandez.display();
+	    Piano.display();
+	    Oliveros.display();
+	    Hallig.display();
+	    Pascua.display();
+	    Smith.display();
+	    Piano.display();
+	    Geringer.display();
+	    ClarkKurt.display();
+	    Hernandez.display();
+	    Piano.display();
+	    KPClark.display();
+	    Bermejo.display();
+	    Santander.display()
 
     }
 }

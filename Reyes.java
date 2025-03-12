cbbed0f93d2fd8e72903aeba6ce3e3f43ccea691
reyes <japreyes@paterostechnologicalcollege.edu.ph>
2025-03-12 20:47:04 +0800

    Added Reyes.java with age calculation and updated week5.java

---
+++ Reyes.java
@@ -0,0 +1,8 @@
+public class Reyes {
+    public static void display() {
+        int age = 20; // Replace with user input if necessary
+        System.out.println("Hello! This is Reyes contributing to the project.");
+        System.out.println("My current age is: " + age);
+        System.out.println("Next year, I will be: " + (age + 1));
+    }
+}
